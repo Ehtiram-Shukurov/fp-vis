4fc6c60085a61fe3e8155a4bb8f0eb0033b11de6
Ehtiram-Shukurov <shuku010@umn.edu>
2026-04-29 00:13:09 -0500

    fix scrollyhist

--- src/lib/components/ScrollyHist.svelte
+++ src/lib/components/ScrollyHist.svelte
@@ -33,7 +33,7 @@
       selectedMovieId: "",
       selectedAge: "All",
       selectedGender: "All",
-      selectedOccupation: "Healthcare",
+      selectedOccupation: "healthcare",
     },
     {
       label: "Unemployed people",
